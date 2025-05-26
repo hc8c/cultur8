94394c600a409a8d1f40c799778c9209c93c8e23
hc8c <213418160+hc8c@users.noreply.github.com>
2025-05-26 19:55:39 +0100

    Updated repo name

--- onionring-variables.js
+++ onionring-variables.js
@@ -6,22 +6,20 @@
 //this file contains the stuff you edit to set up your specific webring
 
 //the full URLs of all the sites in the ring
-var sites = [
-'https://www.thehaughtyculturist.com/',
-];
+var sites = ["https://www.thehaughtyculturist.com/"]
 
 //the name of the ring
-var ringName = 'Cultur8';
+var ringName = "Fliterati"
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'cultur8ring';
+var ringID = "FliteratiWebChain"
 
 //should the widget include a link to an index page?
-var useIndex = false;
+var useIndex = false
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-var indexPage = 'https://hc8c.github.io/cultur8/';
+var indexPage = "https://hc8c.github.io/fliterati/"
 
 //should the widget include a random button?
-var useRandom = true;
+var useRandom = true
