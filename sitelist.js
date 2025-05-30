b01da40199f633d87115ebf35fd01fc438a71d16
hc8c <213418160+hc8c@users.noreply.github.com>
2025-05-30 12:19:02 +0100

    Widget edit

--- sitelist.js
+++ sitelist.js
@@ -4,9 +4,4 @@ export const siteList = [
     siteUrl: "https://www.thehaughtyculturist.com",
     siteDesc: "Culture commentary & story deconstruction",
   },
-  {
-    siteName: "exampler.com",
-    siteUrl: "https://www.exampler.com",
-    siteDesc: "Example description",
-  },
 ]
