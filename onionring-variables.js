baf44b1df112d6f18f0cbe1ab946e5bfa9687725
hc8c <213418160+hc8c@users.noreply.github.com>
2025-05-30 12:36:46 +0100

    widget fixes

--- onionring-variables.js
+++ onionring-variables.js
@@ -8,21 +8,20 @@
 import { siteList } from "./sitelist.js"
 
 //the full URLs of all the sites in the ring
-const sites = []
-siteList.forEach((site) => sites.push(site["siteUrl"]))
+var sites = ["https://www.thehaughtyculturist.com/"]
 
 //the name of the ring
-const ringName = "Fliterati"
+var ringName = "Fliterati"
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-const ringID = "FliteratiWebChain"
+var ringID = "FliteratiWebChain"
 
 //should the widget include a link to an index page?
-const useIndex = true
+var useIndex = true
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-const indexPage = "https://hc8c.github.io/fliterati/"
+var indexPage = "https://hc8c.github.io/fliterati/"
 
 //should the widget include a random button?
-const useRandom = true
+var useRandom = true
